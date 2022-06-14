45c9b1c1f7dd653c28102e9aa1a050dfed773436
chuxi <agustin.vergara.ruiz@gmail.com>
2022-06-14 11:54:10 -0500

    first semi-functional version of the contact app, the index view is working as intended but need to add functionality to the search bar -- contact detail view works as intended -- create new contact view is now visible but i need to polish almost all functionalities

--- contact_agenda/contact/admin.py
+++ contact_agenda/contact/admin.py
@@ -1,3 +1,19 @@
-from django.contrib import admin
-
 # Register your models here.
+
+from django.contrib import admin
+import django.apps
+
+
+#aca usamos un modulo que esta build in en django el cual nos permite fetchear los modelos de models.py y los mnete en una lista (por verificar)
+models = django.apps.apps.get_models()
+for model in models:
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
+
+#probar a hacer mi propia funcion que fetchee los modelos de models.py y
+#los meta en una lista
+#class Admin_site_model:
+#    def __init__(self,)
+#       admin.site.register(Contact)
--- contact_agenda/contact/urls.py
+++ contact_agenda/contact/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name = 'contact'
+
 urlpatterns = [
-    path('', views.index, name='index')
+    #index path ex: contact/
+    path('', views.index, name='index'),
+    #add contact path ex: contact/add-contact/
+    path('add-contact/', views.addContactView, name = 'add-contact'),
+    #contact saver path ex: contact/contactSaver/
+    path('contactSaver/', views.contactSaver, name= 'contactSaver'),
+    #contact profile path ex: contact/contact-detail/
+    path('<int:contact_id>/contact-detail/', views.detail, name='contact-detail')
 ]
