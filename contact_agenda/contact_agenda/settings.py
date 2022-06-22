078143e5301e66eaee3324f907806c145eff2cd0
chuxi <agustin.vergara.ruiz@gmail.com>
2022-06-22 11:42:30 -0500

    making a js static file for the new.html static because i want to use the local storage in the client to save the imputs in new.html when the client clics into the new-contact-address view

--- contact_agenda/contact_agenda/settings.py
+++ contact_agenda/contact_agenda/settings.py
@@ -121,7 +121,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static') ,)
+STATICFILES_DIRS = [
+    '/home/chuxi/Documents/django/contactapp/contact_agenda/static' ,
+    '/home/chuxi/Documents/django/contactapp/contact_agenda/static/js',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
---
+++ contact_agenda/static/js/new.js
@@ -0,0 +1,15 @@
+const initialState = {
+    fullname: '',
+    relationShip: {
+        familly: false,
+        friend: false,
+        work: false,
+    },
+    phoneNumber:'',
+    email: '',
+}
+
+let inputFullname = document.querySelector('.type')
+
+
+console.log(inputFullname)
