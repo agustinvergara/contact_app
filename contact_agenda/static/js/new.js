3550c4ec30a372f997eeb0458790d808e01812bb
chuxi <agustin.vergara.ruiz@gmail.com>
2022-06-22 12:37:55 -0500

    adding functionality to new.js file because when the user introduce values on the fields we whant to save that info into the local storage and then display it when we go back to the view (new-contact.html)

--- contact_agenda/static/js/new.js
+++ contact_agenda/static/js/new.js
@@ -1,15 +1,46 @@
-const initialState = {
-    fullname: '',
-    relationShip: {
-        familly: false,
-        friend: false,
-        work: false,
-    },
+let initialState = {
+    fullname: '',   
     phoneNumber:'',
     email: '',
 }
+const inputFullname = document.getElementById('fullname')
+const inputPhone = document.getElementById('phone')
+const inputEmail = document.getElementById('email')
 
-let inputFullname = document.querySelector('.type')
+
+inputFullname.addEventListener('change', (e)=> {
+    initialState= {
+        ...initialState,
+        fullname: e.target.value
+    }
+    console.log(initialState)
+    window.localStorage.setItem('fullname', initialState.fullname)   
+
+})
+
+inputPhone.addEventListener('change', (e)=> {
+    initialState= {
+        ...initialState,
+        phoneNumber: e.target.value
+    }
+    console.log(initialState)
+    window.localStorage.setItem('phoneNumber', initialState.phoneNumber)   
+})
+inputEmail.addEventListener('change', (e)=> {
+    initialState= {
+        ...initialState,
+        email: e.target.value
+    }
+    console.log(initialState) 
+    window.localStorage.setItem('email', initialState.email)  
+})
+
+if(window.navigation.currentEntry.url == 'http://127.0.0.1:8000/contact/add-contact/'){
+    inputFullname.value = window.localStorage.getItem('fullname')
+    inputEmail.value = window.localStorage.getItem('email')
+    inputPhone.value = window.localStorage.getItem('phoneNumber')
+
+} 
 
 
-console.log(inputFullname)
+console.log(window.navigation.currentEntry.url)
