668efce9533179d4614b69924580106237f7f358
chuxi <agustin.vergara.ruiz@gmail.com>
2022-07-13 23:05:28 -0500

    trying to nest all the serialized data from contact (contact, contact_address, contact_relation) to display it with 1 view

--- contact_agenda/contact/api/views.py
+++ contact_agenda/contact/api/views.py
@@ -12,9 +12,11 @@ from contact.urls import *
 def api_contactserializer_detailview(request, contact_id):#for now im hardcoding contact_id 
     try:
         contact = Contact.objects.get(pk=contact_id)
+        address = ContactAddress.objects.get(contact_id=contact_id)
     except Contact.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET' or 'POST':
         serializer = ContactSerializer(contact)
+        serializer = ContactAdressSerializer(address)
         return Response(serializer.data)
