1cf9448bdedc803b2ffe127bc99aac224f95ed64
chuxi <agustin.vergara.ruiz@gmail.com>
2022-07-19 03:21:00 -0500

    the endpoint that throws all related data of the contact is working in contact/api/views

--- contact_agenda/contact/api/serializers.py
+++ contact_agenda/contact/api/serializers.py
@@ -2,25 +2,18 @@ from dataclasses import field
 from rest_framework import serializers
 from contact.models import *
 
-
 class ContactAdressSerializer(serializers.ModelSerializer):
     class Meta:
         model = ContactAddress
         fields = ['contact', 'address_1', 'address_2', 'city']
 
-
 class ContactSerializer(serializers.Serializer):
-    # class ContactAdressSerializer(serializers.ModelSerializer):
-    #     class Meta:
-    #         model = ContactAddress
-    #         fields = ['contact', 'address_1', 'address_2', 'city']
 
     contact_relation = serializers.PrimaryKeyRelatedField(allow_null=True, queryset=ContactRelation.objects.all(), required=False)
     contact_name = serializers.CharField(max_length=30)
     contact_lastname = serializers.CharField(max_length=30)
     phone_number = serializers.IntegerField(max_value=2147483647, min_value=-2147483648)
     contact_email = serializers.CharField(allow_blank=True, allow_null=True, max_length=40, required=False)
-    contact_address = ContactAdressSerializer()
     
 
 
--- contact_agenda/contact/api/views.py
+++ contact_agenda/contact/api/views.py
@@ -1,4 +1,5 @@
-from rest_framework import status
+from django.shortcuts import get_object_or_404
+from rest_framework import status, viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -7,7 +8,7 @@ from contact.api.serializers import *
 from contact.urls import *
 
 
-
+'''api view to see all the data related to the User(User, UserAddress, UserRelation)'''
 @api_view(['GET', 'POST'])
 def api_contactserializer_detailview(request, contact_id):#for now im hardcoding contact_id 
     try:
@@ -17,6 +18,17 @@ def api_contactserializer_detailview(request, contact_id):#for now im hardcoding
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET' or 'POST':
-        contact_serializer = ContactSerializer.ContactAdressSerializer(address)
-        contact_serializer = ContactSerializer(contact)
-        return Response(contact_serializer.data)
+        def serializer_returner():
+            contact_address_serializer = ContactAdressSerializer(address)
+            contact_serializer = ContactSerializer(contact)
+            return contact_serializer.data, contact_address_serializer.data
+
+        return Response(serializer_returner())
+
+
+
+
+        
+
+    
+    
